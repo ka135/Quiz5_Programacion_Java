7e44b1c99b2987a2b1d7369cb8306184ef69b2aa
ka135 <karolinasuarezl11@gmail.com>
2023-10-04 20:12:39 -0500

    Codigo comentado

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -4,12 +4,29 @@ import java.util.LinkedList;
  * clase Biblioteca
  */
 public class Biblioteca {
+
+    /**
+     * Declaración de una lista que almacena los librosDisponibles
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+     */
     public LinkedList<Libro> librosDisponibles = new LinkedList<Libro>();
 
+    /**
+     * Método para registrar un libro en la biblioteca
+     *
+     * Complejidad temporal: O(1) Tiempo constante
+    */
     public void registrarLibro(Libro libro) {
         librosDisponibles.add(libro);
     }
 
+    /**
+     * Metodo para buscar el libro
+     * Si no encuentra el libro, se devuelve null
+     *
+     * Complejidad temporal: O(N) Operacion de Tiempo lineal
+     */
     public Libro buscarLibro(String titulo) {
         for (int i = 0; i < librosDisponibles.size(); i = i + 1) {
             Libro libro1 = librosDisponibles.get(i);
@@ -20,6 +37,11 @@ public class Biblioteca {
         return null;
     }
 
+    /**
+     * Método para obtener la lista de libros disponibles en la biblioteca
+     *
+     * Complejidad temporal: O(1) Operacion de Tiempo Constante
+     */
     public LinkedList<Libro> getLibrosDisponibles() {
         return librosDisponibles;
     }
--- src/Principal.java
+++ src/Principal.java
@@ -9,7 +9,17 @@ public class Principal {
      * Complejidad temporal: O(1) Tiempo constante
      */
     public static void main(String[] args) {
+        /**
+         * Se crea un objeto de la clase 'Biblioteca' llamada 'bibliotecaKonradLorenz'
+         *
+         * Complejidad temporal: O(1) Tiempo constante
+         */
         Biblioteca bibliotecaKonradLorenz = new Biblioteca();
+        /**
+         * Se crean cinco objetos de la clase 'Libro' con diferentes títulos, autores y número de páginas
+         *
+         * Complejidad temporal: O(1) Tiempo constante
+         */
         Libro libro1 = new Libro("Cienañosdesoledad","GabrielGarcíaMárquez",496);
         Libro libro2 = new Libro("ElcódigoDaVinci","DanBrown",656);
         Libro libro3 = new Libro("1984","GeorgeOrwell",326);
